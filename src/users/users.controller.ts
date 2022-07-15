1e8019c06f3cff9659d1c10a55559874bc0664f7
ParkChaerim <qkrdbsk28@naver.com>
2022-07-15 09:46:54 +0900

    controller prac

---
+++ src/users/users.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+
+@Controller('users')
+export class UsersController {
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+}
