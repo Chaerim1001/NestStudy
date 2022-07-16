89233dc37a887cdb18bde3911d736eeef4319850
ParkChaerim <qkrdbsk28@naver.com>
2022-07-16 22:06:49 +0900

    service prac

---
+++ src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { v4 as uuid } from 'uuid';
+import { Injectable } from '@nestjs/common';
+import { EmailService } from 'src/email/email.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private emailService: EmailService) {}
+
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  private checkUserExists(email: string) {
+    return false; // TODO: DB 연동 후 구현
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return; // TODO: DB 연동 후 구현
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    await this.emailService.sendMemberJoinVerification(
+      email,
+      signupVerifyToken,
+    );
+  }
+
+  async verifyEmail(signupVerifyToken: string): Promise<string> {
+    // TODO
+    // 1. DB에서 signupVerifyToken으로 회원 가입 처리중인 유저가 있는지 조회하고 없다면 에러 처리
+    // 2. 바로 로그인 상태가 되도록 JWT를 발급
+
+    throw new Error('Method not implemented.');
+  }
+}
