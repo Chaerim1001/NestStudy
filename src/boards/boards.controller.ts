8eed4851e6e9f488880103a3b4dcdda2a25a0d16
ParkChaerim <qkrdbsk28@naver.com>
2022-07-26 10:08:34 +0900

    feat: create board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -11,5 +12,10 @@ export class BoardsController {
   @Get()
   getAllTasks(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Board } from './board.model';
+import { BoardStatus } from './boardStatus.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +10,16 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const board: Board = {
+      id: uuid(),
+      ...createBoardDto,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
