d890327efd5725357f992272dbd0cdbd157f7f94
ParkChaerim <qkrdbsk28@naver.com>
2022-07-26 11:18:35 +0900

    add: pipe transform

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -13,6 +13,7 @@ import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatus } from './boardStatus.model';
+import { BoardStatusValidationPipe } from './board.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -44,7 +45,7 @@ export class BoardsController {
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
