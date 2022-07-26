8ec082587fd795d7b7c7cfb2a2312b9c1825769e
ParkChaerim <qkrdbsk28@naver.com>
2022-07-26 09:56:43 +0900

    feat: get all boards

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {
     this.boardsService = boardsService;
+  }
+
+  @Get()
+  getAllTasks(): Board[] {
+    return this.boardsService.getAllBoards();
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; // todo: 실제 DB 연결 후 수정
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+}
